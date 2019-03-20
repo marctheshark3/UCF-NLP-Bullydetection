8d64c2e63b623a72e23c2c8a854a3a790cd5c268
Marc Mailloux <marcmailloux@knights.ucf.edu>
2019-03-20 16:04:39 -0400

    Embeding Updates
    The labels and corpus dont have same number of observations, and I cant get it to output to csv :(

--- Tokenization/Tokenization.py
+++ Tokenization/Tokenization.py
@@ -7,7 +7,7 @@ import time
 
 start = time.time()
 
-print("hello")
+
 #change your file location resprective of your path
 file_location = "/Users/marctheshark/Documents/NLP/Final Project/data.txt"
 
@@ -28,8 +28,9 @@ def preprocessing(your_data):
     corpus = []
 
     #looping over the length of the data
-    for i in range(len(data)):
+    for i in range(1,len(data)):
         index = data[i]
+
 
         #reverse looping through each index to find the label 0,1,2,3
         for j in reversed(range(len(index))):
@@ -37,7 +38,7 @@ def preprocessing(your_data):
 
             try:
                 #if the single_index is 0,1,2,3 lets store it
-                if int(single_index) <= 3:
+                if any(char.isdigit() for char in single_index):
                     sentiment.append(int(single_index))
 
                     #stop this from looping through the rest of the index
@@ -58,8 +59,13 @@ def preprocessing(your_data):
         sw = set(stopwords.words('english'))
         filter_words = [w for w in filter_words if not w in sw]
         corpus.append(filter_words)
-
+        #somehow the labels and corpus is off, easily fixable
     return sentiment, corpus
+x,y = (preprocessing(file_location))
+
+print(len(x))
+print(len(y))
+
 
 def n_gram_GetTraining(data, n):
 
@@ -69,8 +75,10 @@ def n_gram_GetTraining(data, n):
 
     #looking through each sentence in the corpus
     for i in range(len(corpus)):
+
         sentences = corpus[i]
         sentence_ngram =[]
+        #print(sentences)
 
         #looking at each word of the sentence
         for word in range(len(sentences)):
@@ -84,14 +92,32 @@ def n_gram_GetTraining(data, n):
     tokens = []
     for j in range(len(corpus_ngram)):
         index = corpus_ngram[j]
+
         for w in range(len(index)):
             next_index = index[w]
+
+            #if there are unequal ngrams dont use them
+            try:
+              if len(next_index) < n:
+                #print('breaking')
+                break
+            except:
+              w = 0
 
             if next_index not in tokens:
                 tokens.append(next_index)
     tokens.sort()
 
-    return corpus_ngram , labels, tokens
+    encoded_data = np.zeros([len(corpus_ngram), len(tokens)])
+    count = 0
+    for z in range(len(tokens)):
+      unique_token = tokens[z]
+      for e in range(len(corpus_ngram)):
+        if unique_token in corpus_ngram[e]:
+          count += 1
+          encoded_data[e, z] = count
+
+    return encoded_data , labels
 
 
 #data is the location of you file in txt format
@@ -102,9 +128,45 @@ quad_gram = n_gram_GetTraining(file_location,4)
 
 combination_gram = np.hstack((tri_gram, quad_gram))
 
-x , y ,z = quad_gram
+x , y = quad_gram
+print(x)
+print(len(x))
+print(len(y))
+qg = pd.DataFrame(quad_gram)
+print(qg)
+qg.to_csv(path_or_buf= "/Users/marctheshark/Documents/NLP/Final Project/output.csv" , index=False)
+
+#export_csv = quad_gram.to_csv (r"/Users/marctheshark/Documents/NLP/Final Project/outputdata.csv", header=True) #Don't forget to add '.csv' at the end of the path
 
 #might be a good idea to output all three of n_grams so we dont have to reprocess them everytime
 
 end = time.time()
 print(end - start)
+'''  
+from sklearn.model_selection import train_test_split , GridSearchCV , RepeatedKFold
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.svm import SVC
+print(len(x))
+print(len(y))
+X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=333)
+print(len(X_train))
+print(len(y_train))
+
+kf = KFold(n_splits=10)
+
+hyperparameters = [{'kernel': ['rbf'], 'gamma' : [1, .1, .01, 0.001],  'C': [1, 10, 100]},
+                   {'kernel': ['linear'], 'gamma' : [1, .1, .01, 0.001] ,  'C': [1, 10, 100]}]
+
+classifier = GridSearchCV( estimator= svm.SVC(random_state= 333) , param_grid= hyperparameters, cv = kf)
+classifier.fit(X_train, y_train)
+
+means = classifier.cv_results_['mean_test_score']
+stds = classifier.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, classifier.cv_results_['params']):
+    print("%0.4f (+/-%0.03f) for %r" % (mean, std * 2, params))
+print ('')
+print ("The Best Training Score was:" , classifier.best_score_)
+print('')
+print ("The Best Parameters were: " , classifier.best_params_)
+print (' ')
+'''
