98618445e2d37896f96d2bb5468effa62dd456e5
Maxim Shelopugin <maxim.shelopugin@gmail.com>
2019-04-04 15:03:57 -0400

    fixed cross validation

--- Cross_validation.py
+++ Cross_validation.py
@@ -1,89 +1,60 @@
 import random
 from sklearn.cross_validation import KFold
 
+# Create an object which stores validation and test indecies
 class Cross_validation:
 	def __init__(self, n_grams, labels, size):
 		self.validation_size = size
 
+		# Separate each n-gram into list of categories
 		self.categories = [[], [], [], []]
-
 		for i in range(0, len(labels)):
 			self.categories[labels[i]].append(n_grams[i])
 
+		# Double count necessary categories
 		double_count_1 = self.categories[1].copy()
 		self.categories[1]+= double_count_1
 
 		double_count_2 = self.categories[2].copy()
 		self.categories[2]+=double_count_2
 
-		random.shuffle(self.categories[0])
-		random.shuffle(self.categories[1])
-		random.shuffle(self.categories[2])
-		random.shuffle(self.categories[3])
+		# Get the indecies of training and testing sets
+		self.indecies = [[],[],[],[]]
+		for category in range(0,4):
+			self.indecies[category] = KFold(len(self.categories[category]), n_folds=self.validation_size)
 
-		self.indecies1 = KFold(len(self.categories[0]), n_folds=self.validation_size)
-		self.indecies2 = KFold(len(self.categories[1]), n_folds=self.validation_size)
-		self.indecies3 = KFold(len(self.categories[2]), n_folds=self.validation_size)
-		self.indecies4 = KFold(len(self.categories[3]), n_folds=self.validation_size)
+		# Separate eacg of the lists into a category with separate lists for training and testing indecies
+		self.list = [[[],[]],[[],[]],[[],[]],[[],[]]]
+		for category in range(0, 4):
+			for start, end in self.indecies[category]:
+				self.list[category][0].append(start)
+				self.list[category][1].append(end)
 
-		self.list1 = [[],[]]
-		self.list2 = [[],[]]
-		self.list3 = [[],[]]
-		self.list4 = [[],[]]
 
-		for start, end in self.indecies1:
-			self.list1[0].append(start)
-			self.list1[1].append(end)
-
-		for start, end in self.indecies2:
-			self.list2[0].append(start)
-			self.list2[1].append(end)
-
-		for start, end in self.indecies3:
-			self.list3[0].append(start)
-			self.list3[1].append(end)
-
-		for start, end in self.indecies4:
-			self.list4[0].append(start)
-			self.list4[1].append(end)
-
+	# Return the training and testing sets/labels
 	def get_sets(self, k):
 		test_set = []
 		train_set = []
 		test_labels = []
 		train_labels = []
 
-		for index in self.list1[1][k]:
-			test_set.append(self.categories[0][index])
-			test_labels.append(0)
+		for category in range(0, 4):
+			for index in self.list[category][1][k]:
+				test_set.append(self.categories[category][index])
+				test_labels.append(category)
 
-		for index in self.list2[1][k]:
-			test_set.append(self.categories[1][index])
-			test_labels.append(1)
+		for category in range(0, 4):
+			for index in self.list[category][0][k]:
+				train_set.append(self.categories[0][index])
+				train_labels.append(category)
 
-		for index in self.list3[1][k]:
-			test_set.append(self.categories[2][index])
-			test_labels.append(2)
-
-		for index in self.list4[1][k]:
-			test_set.append(self.categories[3][index])
-			test_labels.append(3)
-
-		for index in self.list1[0][k]:
-			train_set.append(self.categories[0][index])
-			train_labels.append(0)
-
-		for index in self.list2[0][k]:
-			train_set.append(self.categories[1][index])
-			train_labels.append(1)
-
-		for index in self.list3[0][k]:
-			train_set.append(self.categories[2][index])
-			train_labels.append(2)
-
-		for index in self.list4[0][k]:
-			train_set.append(self.categories[3][index])
-			train_labels.append(3)
+		#Shuffle the data
+		random.seed(30)
+		random.shuffle(train_set)
+		random.shuffle(test_set)
+		random.seed(30)
+		random.shuffle(train_labels)
+		random.shuffle(test_labels)
 
 		return train_set, train_labels, test_set, test_labels
 
