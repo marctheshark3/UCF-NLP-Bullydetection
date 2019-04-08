14fa01295317a32cdbfca6250758a50106ab7f0d
Rolando Nieves <rolando.j.nieves@nasa.gov>
2019-04-08 17:06:30 -0400

    Integrate cross-validation logic
    Integrate the cross-validation logic written by Maxim. Used it to train
    an ANN on our data set.
    
    Also remove the redundant source files in the root directory.

--- classify/crossval.py
+++ classify/crossval.py
@@ -1,60 +1,63 @@
 import random
-from sklearn.model_selection import KFold
 import numpy as np
+from sklearn.model_selection import KFold
 
+# Create an object which stores validation and test indecies
 class CrossValidation:
   def __init__(self, n_grams, labels, size):
     self.validation_size = size
 
-    self.categories = [[] for _ in range(4)]
-
-    # Sort samples into individual labels
+    # Separate each n-gram into list of categories
+    self.categories = [[], [], [], []]
     for i in range(0, len(labels)):
       self.categories[labels[i]].append(n_grams[i])
 
-    # Shuffle the samples in each label category, while doubling the samples for
-    # labels in [1, 2]
-    labels_to_double = range(1, 3)
-    for cat_idx in range(4):
-      if cat_idx in labels_to_double:
-        self.categories[cat_idx] += self.categories[cat_idx].copy()
-      random.shuffle(self.categories[cat_idx])
+    # Double count necessary categories
+    double_count_1 = self.categories[1].copy()
+    self.categories[1]+= double_count_1
 
-    self.indices = [
-      KFold(
-        n_splits=self.validation_size
-      ).split(self.categories[cat_idx]) for cat_idx in range(4)
-    ]
+    double_count_2 = self.categories[2].copy()
+    self.categories[2]+=double_count_2
 
-    # Build the per-class index lists
-    self.index_lists = [([], []) for _ in range(4)]
-    for cat_idx, cat_indices in enumerate(self.indices):
-      for start, end in cat_indices:
-        self.index_lists[cat_idx][0].append(start)
-        self.index_lists[cat_idx][1].append(end)
+    # Get the indecies of training and testing sets
+    self.indecies = [[],[],[],[]]
+    for category in range(0,4):
+      self.indecies[category] = KFold(n_splits=self.validation_size).split(self.categories[category])
 
+    # Separate eacg of the lists into a category with separate lists for training and testing indecies
+    self.list = [[[],[]],[[],[]],[[],[]],[[],[]]]
+    for category in range(0, 4):
+      for start, end in self.indecies[category]:
+        self.list[category][0].append(start)
+        self.list[category][1].append(end)
+
+
+  # Return the training and testing sets/labels
   def get_sets(self, k):
-    TRAINING_SET = 0
-    TEST_SET = 1
-    FEATURE_VECTOR = 0
-    LABEL = 1
-    data_sets = ([], [],)
+    test_set = []
+    train_set = []
+    test_labels = []
+    train_labels = []
 
-    for trn_test in [TRAINING_SET, TEST_SET,]:
-      for cat_idx in range(4):
-        for index in self.index_lists[cat_idx][trn_test][k]:
-          data_sets[trn_test].append(
-            (self.categories[cat_idx][index], cat_idx,)
-          )
-      random.shuffle(data_sets[trn_test])
-    training_samples = [a_set_tuple[FEATURE_VECTOR] for a_set_tuple in data_sets[TRAINING_SET]]
-    training_labels = [a_set_tuple[LABEL] for a_set_tuple in data_sets[TRAINING_SET]]
-    test_samples = [a_set_tuple[FEATURE_VECTOR] for a_set_tuple in data_sets[TEST_SET]]
-    test_labels = [a_set_tuple[LABEL] for a_set_tuple in data_sets[TEST_SET]]
+    for category in range(0, 4):
+      for index in self.list[category][1][k]:
+        test_set.append(self.categories[category][index])
+        test_labels.append(category)
 
-    return np.array(training_samples), \
-      np.array(training_labels), \
-      np.array(test_samples), \
-      np.array(test_labels)
+    for category in range(0, 4):
+      for index in self.list[category][0][k]:
+        train_set.append(self.categories[0][index])
+        train_labels.append(category)
+
+    #Shuffle the data
+    random.seed(30)
+    random.shuffle(train_set)
+    random.shuffle(test_set)
+    random.seed(30)
+    random.shuffle(train_labels)
+    random.shuffle(test_labels)
+
+    return np.array(train_set), np.array(train_labels), np.array(test_set), np.array(test_labels)
 
 # vim: set ts=2 sw=2 expandtab:
+
