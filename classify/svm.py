b2f0e3b1f0d7cf5baa137793008346049d08c871
Marc Mailloux <marcmailloux@knights.ucf.edu>
2019-04-11 10:47:31 -0400

    SVM Updates
    fixed the issue where the metric was only for binary labels now it is for multiple predictors

--- classify/svm.py
+++ classify/svm.py
@@ -10,20 +10,20 @@ def get_svm_metrics(model,train_data,train_labels,test_data,test_labels):
   model_prediction = model.predict(test_data)
 
   #getting metrics
-  acc = metrics.accuracy_score(test_labels, model_prediction)
-  cm = metrics.confusion_matrix(test_labels,model_prediction)
-  recall = metrics.recall_score(test_labels,model_prediction)
-  precision = metrics.precision_score(test_labels,model_prediction)
+  #acc = metrics.accuracy_score(test_labels, model_prediction)
+  #cm = metrics.confusion_matrix(test_labels,model_prediction)
+  #recall = metrics.recall_score(test_labels,model_prediction)
+  #precision = metrics.precision_score(test_labels,model_prediction)
   report = metrics.classification_report(test_labels,model_prediction)
 
   print("The results of the",model, "model are:")
-  print("accuracy:", acc)
-  print("recall:", recall)
-  print("precision", precision)
-  print("The confusion matrix:", cm)
+  #print("accuracy:", acc)
+  #print("recall:", recall)
+  #print("precision", precision)
+  #print("The confusion matrix:", cm)
   print("Lastly the Classification Report:", report)
 
-  return acc
+  return report
 
   # vim: set ts=2 sw=2 expandtab:
 
